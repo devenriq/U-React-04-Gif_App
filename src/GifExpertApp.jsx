07ab1a70af4ff1502eb4949e71ec1437bece95e5
enrique palomino <octepc.97@gmail.com>
2023-02-12 18:59:36 -0500

    made a component to search info

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch", "Naruto"]);
+
+  const onAddCategory = () => {
+    setCategories([...categories, "hola"]);
+  };
+
+  console.log(categories);
+
   return (
     <>
       <h1>GifExpertApp</h1>
+
+      <AddCategory />
+
+      {/* Listado de gifs */}
+      <button onClick={onAddCategory}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };
