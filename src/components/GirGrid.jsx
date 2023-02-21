c3f1a72f9f12b047ae348eb46a29f8c88c883888
enrique palomino <octepc.97@gmail.com>
2023-02-20 21:12:11 -0500

    made a index from which all the components can be exported

--- src/components/GirGrid.jsx
+++ src/components/GirGrid.jsx
@@ -1,26 +1,19 @@
-import { useEffect, useState } from "react";
-import { getGifs } from "../helpers/getGifs";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GifItem } from "./GifItem";
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  const getImages = async () => {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  };
-
-  useEffect(() => {
-    getImages();
-  }, []);
+  const { images, isLoading } = useFetchGifs(category);
 
   return (
     <>
-      {images.map(({ id, title, url }) => (
-        <div className="card" key={id}>
-          <h3>{title}</h3>
-          <img src={url} alt="" />
-        </div>
-      ))}
+      <h3>{category}</h3>
+      {isLoading && <h2>Cargando...</h2>}
+
+      <div className="card-grid">
+        {images.map((image) => (
+          <GifItem key={image.id} {...image} />
+        ))}
+      </div>
     </>
   );
 };
