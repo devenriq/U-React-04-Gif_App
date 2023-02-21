6b1712e80ddff6cadf986a3867ec8db44233ddbf
enrique palomino <octepc.97@gmail.com>
2023-02-21 14:30:09 -0500

    made the helper test

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
 describe("Pruebas en GifItem component", () => {
@@ -8,5 +8,20 @@ describe("Pruebas en GifItem component", () => {
   test("should match with snapshot", () => {
     const { container } = render(<GifItem title={title} url={url} />);
     expect(container).toMatchSnapshot();
+  });
+
+  test("should show the url of the image and its alt", () => {
+    render(<GifItem title={title} url={url} />);
+    // screen.debug();
+    const { src, alt } = screen.getByRole("img");
+
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+  });
+
+  test("should show the title of the component", () => {
+    render(<GifItem title={title} url={url} />);
+    expect(screen.getByText(title)).toBeTruthy();
+    screen.debug();
   });
 });
